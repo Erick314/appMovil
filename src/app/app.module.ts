26c8410636cd5221de7e224bd3f699f0ae9c8d9d
Erick <eric.paredes@duocuc.cl>
2024-08-30 00:28:17 -0400

    Login y home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { LoginComponent } from './login/login.component';
-
+//login
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { HomeComponent } from './home/home.component';
 
+//home
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
+  declarations: [AppComponent, LoginComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -25,6 +31,17 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     MatInputModule,
     MatButtonModule,
     MatFormFieldModule,
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSnackBarModule,
   ],
   providers: [provideClientHydration(), provideAnimationsAsync()],
   bootstrap: [AppComponent],
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent implements OnInit {
+  infoForm: FormGroup;
+  username = 'user@example.com'; // Este valor debería ser pasado desde el servicio de autenticación
+  nivelesEducacion = [
+    'Básica',
+    'Media',
+    'Universitaria',
+    'Técnico',
+    'Doctorado',
+  ];
+
+  constructor(private fb: FormBuilder, private snackBar: MatSnackBar) {
+    this.infoForm = this.fb.group({
+      nombre: [''],
+      apellido: [''],
+      educacion: [''],
+      fechaNacimiento: [''],
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onClear() {
+    this.infoForm.reset();
+  }
+
+  onShow() {
+    const { nombre, apellido } = this.infoForm.value;
+    this.snackBar.open(`Nombre: ${nombre}, Apellido: ${apellido}`, 'Close', {
+      duration: 5000,
+    });
+  }
+}
