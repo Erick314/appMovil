f6e9a1b0304cdce874010d9dbe8327ae4a6e81bf
jordanOsorio <jo.osoriob@duocuc.cl>
2024-09-04 19:49:57 -0400

    Se agrega recuperar contraseña en login, y se agrega cerrar sesion en el principal

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,6 +1,6 @@
 import { Component,ViewChild  } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-principal',
@@ -9,10 +9,16 @@ import { MatSidenav } from '@angular/material/sidenav';
 })
 export class PrincipalComponent {
   @ViewChild('sidenav') sidenav?: MatSidenav;
+  
+  constructor(private router: Router) {}
 
   toggleSidenav() {
     if (this.sidenav) {
       this.sidenav.toggle();
     }
+  }
+  logout() {
+    // Aquí podrías agregar la lógica para cerrar sesión si es necesario
+    this.router.navigate(['/login']);
   }
 }
